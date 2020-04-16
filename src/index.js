233f6a2719a776543f64e14fd64c5389ed8936cb
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2020-04-16 21:21:30 +0900

    Debugging

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const core = require('@actions/core');
 const { Installer } = require('./installer');
 
 (async () => {
-  const installer = new Installer(core.getInput('version'), 'src');
+  const installer = new Installer(core.getInput('version'), __dirname);
   try {
     await installer.install();
   } catch (e) {
