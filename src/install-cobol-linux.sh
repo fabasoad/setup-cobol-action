a67abd648311d63614a6b9141e20898205f71054
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2020-04-16 22:04:10 +0900

    Add sudo to commands

--- src/install-cobol-linux.sh
+++ src/install-cobol-linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get -y upgrade
+sudo apt-get -y upgrade # TODO: check without this command
 sudo apt-get -y install curl tar libncurses5-dev libgmp-dev libdb-dev
 sudo apt-get -y autoremove
 sudo apt-get -y install iproute2
@@ -8,11 +8,11 @@ sudo apt-get -y install iproute2
 # download and install open-cobol for depencies (libcob >= 4.0)
 sudo apt-get -y install ranger autoconf build-essential
 curl -sLk https://sourceforge.net/projects/open-cobol/files/gnu-cobol/3.0/gnucobol-"${1}".tar.gz | tar xz
-cd gnucobol-"${1}" && ./configure --prefix=/usr && make && make install && ldconfig && cd /tmp/ && rm -rf ./*
+cd gnucobol-"${1}" && ./configure --prefix=/usr && sudo make && sudo make install && sudo ldconfig && cd /tmp/ && rm -rf ./*
 sudo apt-get -y --purge autoremove
 
-chmod +x /home/cobol/cobol-unit-test/run-ut
-chmod +x /home/cobol/cobol-unit-test/compile
-chmod +x /home/cobol/cobol-unit-test/upload/ZUTZCPC
+sudo chmod +x /home/cobol/cobol-unit-test/run-ut
+sudo chmod +x /home/cobol/cobol-unit-test/compile
+sudo chmod +x /home/cobol/cobol-unit-test/upload/ZUTZCPC
 
 echo "::add-path::/home/cobol"
