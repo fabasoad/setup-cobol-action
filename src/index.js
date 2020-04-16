acaed882fd345cadec8909156bbbd04601d2cf9a
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2020-04-16 21:54:32 +0900

    Fix path to script

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const core = require('@actions/core');
+const path = require('path');
 const { Installer } = require('./installer');
 
 (async () => {
-  const installer = new Installer(core.getInput('version'), '../src');
+  const installer = new Installer(core.getInput('version'), path.join(__dirname, '../src'));
   try {
     await installer.install();
   } catch (e) {
