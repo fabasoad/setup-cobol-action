a58b745065e047addf22f8f1303b1055454a258b
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2020-04-16 21:57:04 +0900

    Update Shell file

--- src/install-cobol-linux.sh
+++ src/install-cobol-linux.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-apt-get update
-apt-get -y upgrade
-apt-get -y install curl tar libncurses5-dev libgmp-dev libdb-dev
-apt-get -y autoremove
-apt-get -y install iproute2
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y install curl tar libncurses5-dev libgmp-dev libdb-dev
+sudo apt-get -y autoremove
+sudo apt-get -y install iproute2
 
 # download and install open-cobol for depencies (libcob >= 4.0)
-apt-get -y install ranger autoconf build-essential
+sudo apt-get -y install ranger autoconf build-essential
 curl -sLk https://sourceforge.net/projects/open-cobol/files/gnu-cobol/3.0/gnucobol-"${1}".tar.gz | tar xz
 cd gnucobol-"${1}" && ./configure --prefix=/usr && make && make install && ldconfig && cd /tmp/ && rm -rf ./*
-apt-get -y --purge autoremove
+sudo apt-get -y --purge autoremove
 
 chmod +x /home/cobol/cobol-unit-test/run-ut
 chmod +x /home/cobol/cobol-unit-test/compile
