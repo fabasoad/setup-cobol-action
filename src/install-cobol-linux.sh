b4761d0a55b5332e80c9324183c7534b0afd222e
Sven Pfennig <s.pfennig@reply.de>
2022-07-26 19:42:37 +0200

    [Version]: Set '3.1.2' as default version. Add versions: '3.1-rc1', '3.1.1', '3.1.2'.
    Signed-off-by: Sven Pfennig <s.pfennig@reply.de>
    Co-authored-by: Yevhen Fabizhevskyi <fabasoad@gmail.com>

--- src/install-cobol-linux.sh
+++ src/install-cobol-linux.sh
@@ -6,8 +6,8 @@ sudo apt-get -y install iproute2
 
 # download and install open-cobol for depencies (libcob >= 4.0)
 sudo apt-get -y install ranger autoconf build-essential
-curl -sLk https://sourceforge.net/projects/open-cobol/files/gnu-cobol/3.0/gnucobol-"${1}".tar.gz | tar xz
-cd gnucobol-"${1}" && ./configure --prefix=/usr && sudo make && sudo make install && sudo ldconfig && cd /tmp/ && rm -rf ./*
+curl -sLk https://sourceforge.net/projects/open-cobol/files/gnu-cobol/"${1}"/gnucobol-"${2}".tar.gz | tar xz
+cd gnucobol-"${2}" && ./configure --prefix=/usr && sudo make && sudo make install && sudo ldconfig && cd /tmp/ && rm -rf ./*
 sudo apt-get -y --purge autoremove
 
 sudo chmod +x /home/cobol/cobol-unit-test/run-ut
