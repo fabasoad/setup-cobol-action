b8e111b7b923054515f453a5cee2d6c557f8082c
fabasoad <fabasoad@gmail.com>
2025-01-08 00:35:29 +0900

    Debug 2

--- src/install-cobc.sh
+++ src/install-cobc.sh
@@ -12,6 +12,7 @@ main() {
   # download and install open-cobol for dependencies (libcob >= 4.0)
   sudo apt-get -y install ranger autoconf build-essential
 
+  mkdir -p "${bin_path}"
   minor_version="$(echo "${input_version}" | cut -b1- | cut -b-3)"
   url="https://sourceforge.net/projects/open-cobol/files/gnu-cobol/${minor_version}/gnucobol-${input_version}.tar.gz"
   curl -sLk "${url}" -o "${bin_path}/gnucobol.tar.gz"
